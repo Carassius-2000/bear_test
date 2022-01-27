a80868d523d27c7d30ac202710d209afb259155e
Carassius-2000 <skaratsev@gmail.com>
2022-01-27 09:10:54 +0300

    add Select form

--- test.py
+++ test.py
@@ -30,16 +30,55 @@ class AuthorizationWindow(wx.Frame):
 
         box_sizer.Add(flex_grid_sizer, flag=wx.EXPAND | wx.ALL, border=10)
 
-        enter_button = wx.Button(panel, label='Войти', size=(170, 30))
+        enter_button = wx.Button(panel, label='Войти')
         box_sizer.Add(enter_button,
                       flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=10)
 
         panel.SetSizer(box_sizer)
 
 
+class SelectDataWindow(wx.Frame):
+    def __init__(self, parent):
+        super().__init__(parent=parent, title='Выбрать дату прогноза', size=(320, 170))
+        self.Center()
+
+        font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
+        font.SetPointSize(12)
+
+        panel = wx.Panel(self)
+        panel.SetFont(font)
+        panel.SetBackgroundColour('#ee0588')
+
+        box_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        flex_grid_sizer = wx.FlexGridSizer(2, 2, 10, 10)
+
+        date_begin_label = wx.StaticText(panel, label='C')
+        date_begin_edit = wx.TextCtrl(panel, size=(260, 30))
+
+        date_end_label = wx.StaticText(panel, label='По')
+        date_end_edit = wx.TextCtrl(panel, size=(260, 30))
+
+        flex_grid_sizer.AddMany([(date_begin_label),
+                                 (date_begin_edit, wx.ID_ANY, wx.EXPAND),
+                                 (date_end_label),
+                                 (date_end_edit, wx.ID_ANY, wx.EXPAND)])
+
+        box_sizer.Add(flex_grid_sizer, flag=wx.EXPAND | wx.ALL, border=10)
+        
+        enter_button = wx.Button(panel, label='Спрогнозировать')
+        box_sizer.Add(enter_button,
+                      flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=10)
+        
+        panel.SetSizer(box_sizer)
+
+
 app = wx.App()
 
-frame = AuthorizationWindow(None)
-frame.Show()
+# main_frame = AuthorizationWindow(None)
+# main_frame.Show()
+
+select_frame = SelectDataWindow(None)
+select_frame.Show()
 
 app.MainLoop()
